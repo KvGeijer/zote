d5e57383fd2457adce0ec442a5e8f7a73b64a7d9
Kåre von Geijer <kare.kvg@gmail.com>
2023-04-11 20:47:11 +0200

    Fix so that variables are maintained in REPL

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,15 +1,32 @@
 use crate::{code_loc::CodeLoc, errors::ErrorReporter, parser::StmtNode};
 
+use environment::Environment;
+
 mod environment;
 mod expressions;
 mod statements;
 
 type RuntimeError = (CodeLoc, CodeLoc, String);
 
-pub fn interpret(program: &Vec<StmtNode>, error_reporter: &mut ErrorReporter) {
-    let mut env = environment::Environment::new();
+pub struct InterpreterState {
+    env: Environment<'static>,
+}
+
+impl InterpreterState {
+    pub fn new() -> Self {
+        Self {
+            env: Environment::new(),
+        }
+    }
+}
+
+pub fn interpret(
+    program: &Vec<StmtNode>,
+    error_reporter: &mut ErrorReporter,
+    env: &mut InterpreterState,
+) {
     for stmt in program {
-        match statements::eval(stmt, &mut env) {
+        match statements::eval(stmt, &mut env.env) {
             Ok(_) => continue,
             Err((start, end, reason)) => {
                 return error_reporter.runtime_error(&start, &end, &reason)
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use clap::Parser;
+use errors::ErrorReporter;
+use interpreter::InterpreterState;
 use std::fs;
 use std::io::{stdin, stdout, Write};
 use std::process::exit;
@@ -28,9 +30,10 @@ fn main() {
 // Maybe move these out to separate file for running and keeping state
 fn run_file(file: &str) {
     let script = fs::read_to_string(file).expect("Could not open file.");
-    let error_reporter = &mut errors::ErrorReporter::new();
+    let mut error_reporter = errors::ErrorReporter::new();
+    let mut state = InterpreterState::new();
 
-    run(&script, error_reporter);
+    run(&script, &mut error_reporter, &mut state);
 
     if error_reporter.had_compilation_error {
         exit(65);
@@ -43,6 +46,7 @@ fn run_repl() {
     let reader = stdin();
     let mut line = String::new();
     let mut error_reporter = errors::ErrorReporter::new();
+    let mut state = InterpreterState::new();
 
     while {
         print!("> ");
@@ -51,19 +55,15 @@ fn run_repl() {
         reader.read_line(&mut line).unwrap_or(0) > 0
     } {
         // Does not preserve program state between calls
-        run(&line, &mut error_reporter);
+        run(&line, &mut error_reporter, &mut state);
         error_reporter.reset();
     }
 }
 
-fn run(code: &str, error_reporter: &mut errors::ErrorReporter) {
+fn run(code: &str, error_reporter: &mut ErrorReporter, state: &mut InterpreterState) {
     let tokens = scanner::tokenize(code, error_reporter);
 
-    // for token in tokens.iter() {
-    //     println!("{:?}", token);
-    // }
-
     if let Some(stmts) = parser::parse(&tokens, error_reporter) {
-        interpreter::interpret(&stmts, error_reporter);
+        interpreter::interpret(&stmts, error_reporter, state);
     }
 }

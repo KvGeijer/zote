d97426f177de9f55b415baeb812c60345f8a1fee
Kåre von Geijer <kare.kvg@gmail.com>
2023-05-22 14:01:13 +0200

    Add aoc 2022 day 2
    Not the prettiest solution, here are some wanted features:
    * Pattern matching against constants
    * Pattern matching against iterables as lvalues
    * Converting a string/char to ascii int
    * Easier maps:
      * I want map(x -> x >> ...) to just be something like
        map(>> ...) or map(| ...) or similar.
    * Maybe a way to pipe into any position of the argument?
      * Like `func >>1 map(data)` = `map(data, func)`? Easy fix...

--- benches/ast.rs
+++ benches/ast.rs
@@ -28,3 +28,9 @@ fn aoc_2022_1(bench: &mut Bencher) {
     let code = include_str!("../aoc-2022/day01.zote");
     bench.iter(|| run_str(code));
 }
+
+#[bench]
+fn aoc_2022_2(bench: &mut Bencher) {
+    let code = include_str!("../aoc-2022/inputs/02.txt");
+    bench.iter(|| run_str(code));
+}

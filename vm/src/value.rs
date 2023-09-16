4984ca01896625fcaf176713cbc03f9571c45492
Kåre von Geijer <kare.kvg@gmail.com>
2023-09-16 13:44:12 +0200

    Add truthiness conversion

--- vm/src/value.rs
+++ vm/src/value.rs
@@ -1,3 +1,5 @@
+use crate::error::RunRes;
+
 // OPT: Pack as bytesting instead? Very inefficiently stored now in 128 bits
 #[derive(Debug, PartialEq, Clone)]
 pub enum Value {
@@ -5,4 +7,15 @@ pub enum Value {
     Bool(bool),
     Int(i64),
     Float(f64),
+}
+
+impl Value {
+    pub fn truthy(&self) -> RunRes<bool> {
+        match self {
+            Value::Nil => Ok(false),
+            Value::Bool(bool) => Ok(bool),
+            Value::Int(x) => Ok(x != 0),
+            Value::Float(x) => Ok(x != 0.0), // not very useful
+        }
+    }
 }

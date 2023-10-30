217090d1b6ec78cebccae33aea8fecd4996faf32
Kåre von Geijer <kare.kvg@gmail.com>
2023-10-30 21:19:26 +0100

    Add documentation of the bytecode

--- vm/src/compiler/bytecode.rs
+++ vm/src/compiler/bytecode.rs
@@ -3,32 +3,112 @@ use enum_macros::TryFromByte;
 /// A byte opcode describes what the coming bytes on in a stack are
 #[derive(TryFromByte, Debug)]
 pub enum OpCode {
+    /// Returns from a function
+    ///
+    /// Resets VM poiters to before the call, but keeping the return value on temp stack.
+    /// Does discard all values on temp stack belonging to the call.
     Return,
+
+    /// Pushes a constant to the temp stack
+    ///
+    /// The offset into the constant region is read from the next u8
     Constant,
+
+    /// Special case of constant, for the NIL value
     Nil,
+
+    /// Special case of constant, for the True bool
     True,
+
+    /// Special case of constant, for the False bool
     False,
+
+    /// Arithmetically negates the topmost value on the temp stack
     Negate,
+
+    /// Logically negates the topmost value on the temp stack
     Not,
+
+    /// Adds the top two value on the temp stack
     Add,
+
+    /// Subtracts the topmost value on the temp stack from the second top-most value
     Subtract,
+
+    /// Multiplies the top two value on the temp stack
     Multiply,
+
+    /// Divides the second top-most value on the temp stack by the top value
     Divide,
+
+    /// Modulo of the second top-most value on the temp stack by the top value
     Modulo,
+
+    /// Raises the second top-most value on the temp stack to the power of the top value
     Power,
+
+    /// Checks the two top-most values on the temp-stacks for equality
     Equality,
+
+    /// Checks the two top-most values on the temp-stacks for non-equality
     NonEquality,
+
+    /// Pushes whether the second top-most value is less than the top value
     LessThan,
+
+    /// Pushes whether the second top-most value is less or equal to the top value
     LessEqual,
+
+    /// Pushes whether the second top-most value is greater than the top value
     GreaterThan,
+
+    /// Pushes whether the second top-most value is greater or equal to the top value
     GreaterEqual,
+
+    /// Assigns to a global variable
+    ///
+    /// Reads the offset of the global variable from the next bytecode byte.
+    /// The value is read from the temp stack, and it is kept on the stack.
     AssignGlobal,
+
+    /// Reads a global variable onto the temp stack
+    ///
+    /// Reads the offset of the global variable from the next bytecode byte.
     ReadGlobal,
+
+    /// Assigns to a local variable
+    ///
+    /// Reads the offset of the local variable from the rbp from the next bytecode byte.
+    /// The value is read from the temp stack, and it is kept on the stack.
     AssignLocal,
+
+    /// Reads a local variable onto the temp stack
+    ///
+    /// Reads the offset of the variable from the rbp from the next bytecode byte.
     ReadLocal,
+
+    /// Jumps if the top value is false
+    ///
+    /// The jump offset for the pc is read as the next i16 in bytecode.
+    /// The top value on the temp stack is consumed.
     JumpIfFalse,
+
+    /// Jumps with target offset
+    ///
+    /// The jump offset for the pc is read as the next i16 in bytecode.
     Jump,
+
+    /// Discards the top value on the temp stack
     Discard,
+
+    /// Calls the top value on the temp stack
+    ///
+    /// Can be both a closure, and a builtin function.
+    /// In case of a closure, it will create a new call-frame, and start exucuting
+    /// the bytecode for the function. It will still have access to the same globals,
+    /// but not local variables.
     Call,
-    FromTemp, // Moves a value from the temporary stack to the ordinary one
+
+    /// Moves the top value on the temp stack to the ordinary stack
+    FromTemp,
 }

5e19f6e099829a32d3318028e743055e17a63683
Kåre von Geijer <kare.kvg@gmail.com>
2023-11-28 22:06:41 +0100

    Solve aoc 2022-6 painlessly
    Maybe we should still have the list function?

--- tests/vm-aoc-tests.rs
+++ tests/vm-aoc-tests.rs
@@ -41,3 +41,8 @@ fn vm_aoc_2022_5() {
     let output = interpret_day("day05");
     assert_eq!(output, "ZWHVFWQWW\nHZFZCCWWV\n");
 }
+#[test]
+fn vm_aoc_2022_6() {
+    let output = interpret_day("day06");
+    assert_eq!(output, "1723\n3708\n");
+}
